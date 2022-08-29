a6bbe6472d224547a3f76d227dd65b4f164ceb6c
armantorkzaban <armantorkzaban@icloud.com>
2022-08-30 01:08:05 +0200

    Tests pass, aside from ChileKinked.geojson

--- test/sweepline_intersections_test.dart
+++ test/sweepline_intersections_test.dart
@@ -26,66 +26,45 @@ void main() {
     var inDir1 = Directory('./test/fixtures/notSimple/');
     for (var file in inDir1.listSync(recursive: true)) {
       if (file is File && file.path.endsWith('.geojson')) {
-        test(file.path, () {
-          var inSource = file.readAsStringSync();
-          var geojson = GeoJSONObject.fromJson(jsonDecode(inSource));
-          var ips = sweeplineIntersections(geojson);
-          print(ips.length);
-          print((geojson as Feature).properties!["expectedIntersections"]);
-          expect(
-              ips.length == geojson.properties!["expectedIntersections"], true);
-        });
+        test(
+          file.path,
+          () {
+            var inSource = file.readAsStringSync();
+            var geojson = GeoJSONObject.fromJson(jsonDecode(inSource));
+            var ips = sweeplineIntersections(geojson);
+            expect(
+                ips.length ==
+                    (geojson as Feature).properties!["expectedIntersections"],
+                isTrue);
+          },
+        );
       }
     }
+
+    File example1 = File('./test/fixtures/notSimple/example.geojson');
+    test(
+      'input data is not modified',
+      () {
+        var inSource = example1.readAsStringSync();
+        var geojson = GeoJSONObject.fromJson(jsonDecode(inSource));
+
+        var clonedData = GeoJSONObject.fromJson(geojson.toJson());
+        sweeplineIntersections(geojson);
+
+        expect(geojson, equals(clonedData));
+      },
+    );
+    test('ignoreSelfIntersections param works', () {
+      var geojson = GeoJSONObject.fromJson(jsonDecode(
+          File('./test/fixtures/notSimple/example.geojson')
+              .readAsStringSync()));
+      var selfIntersectionsIgnored = sweeplineIntersections(geojson, true);
+      expect(selfIntersectionsIgnored, isEmpty);
+
+      var intersections = sweeplineIntersections(geojson, false);
+      expect(intersections.length == 3, true);
+      expect(intersections[0],
+          Position.of([19.88085507071179, -9.98118374351003]));
+    });
   });
 }
-
-
-/*
-import test from 'ava'
-import path from 'path'
-import glob from 'glob'
-
-import load from 'load-json-file'
-
-import sweepline from '../src/main.js'
-
-const trueFixtures = glob.sync(path.join(__dirname, 'fixtures', 'simple', '*.geojson'))
-const falseFixtures = glob.sync(path.join(__dirname, 'fixtures', 'notSimple', '*.geojson'))
-
-test('simple fixtures', (t) => {
-    trueFixtures.forEach((filepath) => {
-        const name = path.parse(filepath).name
-        const geojson = load.sync(filepath)
-        const ips = sweepline(geojson)
-        t.is(ips.length, 0,  `[true] ${name}`)
-    })
-})
-
-
-test('complex fixtures', (t) => {
-    falseFixtures.forEach((filepath) => {
-        const name = path.parse(filepath).name
-        const geojson = load.sync(filepath)
-        const ips = sweepline(geojson)
-        t.deepEqual(ips.length, geojson.properties.expectedIntersections,  `[complex] ${name}`)
-    })
-})
-
-test('input data is not modified', (t) => {
-    const geojson = load.sync(path.join(__dirname, 'fixtures', 'notSimple', 'example.geojson'))
-    const clonedData = JSON.parse(JSON.stringify(geojson))
-    sweepline(geojson)
-    t.deepEqual(geojson, clonedData)
-})
-
-test('ignoreSelfIntersections param works', (t) => {
-    const geojson = load.sync(path.join(__dirname, 'fixtures', 'notSimple', 'example.geojson'))
-    const selfIntersectionsIgnored = sweepline(geojson, true)
-    t.is(selfIntersectionsIgnored.length, 0)
-
-    const intersections = sweepline(geojson, false)
-    t.is(intersections.length, 3)
-    t.deepEqual(intersections[0], [19.88085507071179, -9.98118374351003])
-})
-*/
